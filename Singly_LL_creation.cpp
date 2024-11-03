f211b80bda00e71dd25553d9a1e3e04fb478ddd7
Shashank Sinha <shashanksinha590@gmail.com>
2024-11-03 19:11:00 +0530

    3-11-2024

---
+++ Singly_LL_creation.cpp
@@ -0,0 +1,37 @@
+//Constant time for interting node at end
+#include<iostream>
+using namespace std;
+class Node{//class declaration
+    public://access modifier
+    int data;//data member to hold data in node
+    Node *next;//next pointer variavble of Node type to hold address of another node
+    Node(int data){//constructor to initialize the value if sigantura matches
+        this->data=data;//constructor's initialized data value
+        this->next=NULL;//constructor's initialized next value
+    }
+};
+Node* insertNodeAtend(Node* temp,int data){//function to insert node at end woth O(1)
+    Node* node=new Node(data);
+    //dynamic memory allocation of Node(with data) and variable node pointer of type Node stores data
+    temp->next=node;//linking previous node's next pointer to the newly created node
+    return node;//returning the node pointer 
+}
+void print(Node *head){
+    while(head!=NULL){
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+    cout<<"NULL";
+}
+int main(){    //main function
+    Node* head;//declating head
+    head->data=1;//initializing head's data
+    head->next=NULL;//initializing head's next;
+    Node* temp=head;//another pointer variable temp of type Node to store head's value
+    temp=insertNodeAtend(temp,2);//inserting at end
+    temp=insertNodeAtend(temp,3);
+    temp=insertNodeAtend(temp,1);
+    temp=head;
+    print(temp);
+    return 0;
+}
