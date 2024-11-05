5e3519bd0427ce1f27524e52614a9fe36fdcb564
Shashank Sinha <shashanksinha590@gmail.com>
2024-11-05 21:04:20 +0530

    05-11-2024

--- InsertAtposition_LL.cpp
+++ InsertAtposition_LL.cpp
@@ -9,15 +9,42 @@ class Node{
         next=NULL;
     }
 };
-void InsertAtEnd(Node* &head,int value){
+Node* InsertAtEnd(Node* &temp,int value){
     Node* node=new Node(value);
-    if(head==NULL){
-        head=node;
-        return;
+    temp->next=node;
+    temp=temp->next;
+    return temp;
+}
+void InsertAtPosition(Node* &head,int position,int value){
+    Node* temp=head;
+    int count=1;
+    while(temp!=NULL){
+        count++;
+        if(count==position){
+            Node* node=new Node(value);
+            node->next=temp->next;
+            temp->next=node;
+        }
+        temp=temp->next;
     }
-
+}
+void display(Node* head){
+    while(head!=NULL){
+        cout<<head->data<<"->";
+        head=head->next;
+    }
+    cout<<"NULL"<<endl;
 }
 int main(){
-    
+    Node* head=new Node(10000);
+    Node* temp=head;
+    temp=InsertAtEnd(temp,1);
+    temp=InsertAtEnd(temp,2);
+    temp=InsertAtEnd(temp,5);
+    temp=InsertAtEnd(temp,3);
+    temp=head;
+    display(temp);
+    InsertAtPosition(head,4,4);
+    display(head);
     return 0;
 }
