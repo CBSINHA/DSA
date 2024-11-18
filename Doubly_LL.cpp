0c93b73a7c016edea6b723a61b18ae66d5a7fbce
Shashank Sinha <shashanksinha590@gmail.com>
2024-11-18 20:56:10 +0530

    Doubly LL work 18-11-2024

--- Doubly_LL.cpp
+++ Doubly_LL.cpp
@@ -45,6 +45,40 @@ void insertAthead(Node* &head,int value){
     head->prev=node;
     head=node;
 }
+void insertAtPosition(Node* &head,int value, int pos){
+    Node* temp=head;
+    int count=1;
+    Node* node=new Node(value);
+    if(temp==NULL){
+        head=node;
+    }
+    while(temp!=NULL){
+        if(count==pos){
+            break;
+        }
+        count++;
+        temp=temp->next;
+    }
+        temp->prev->next=node;
+        node->prev=temp->prev;
+        node->next=temp;
+        temp->prev=node;
+}
+void deleteAtPosition(Node* &head,int pos){
+    Node* temp=head;
+    int count=1;
+    while(temp!=NULL){
+        if(count==pos){
+            break;
+        }
+        count++;
+        temp=temp->next;
+    }
+    temp->prev->next=temp->next;
+    temp->next->prev=temp->prev;
+    delete temp;
+    temp=NULL;
+}
 int main()
 {
     Node* head=NULL;
@@ -56,5 +90,11 @@ int main()
     display(head);
     cout<<endl;
     displayUsingprev(head);
+    cout<<endl;
+    insertAtPosition(head,4000,2);
+    display(head);
+    cout<<endl;
+    deleteAtPosition(head,2);
+    display(head);
  return 0;
 }
